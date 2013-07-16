aca224b01ba7bb4c788d7a84cc4e4cc55815b029
nicka1711 <nicka1711@gmail.com>
2013-07-16 18:08:04 +0700

    modify comment facebook direct url

--- wp-content/themes/foreva/comments.php
+++ wp-content/themes/foreva/comments.php
@@ -21,4 +21,19 @@
     $('#commentform').addClass("comment-post");
     $('#submit').addClass("btn-print-gradient");
 </script>-->
-<?php //endif;?>
+<?php  //endif;?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/vn_VN/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="testimonials">
+    <h4 style="padding-bottom: 10px;">Ý kiến bạn đọc:</h4>
+    <?php
+    $link= "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    ?>
+    <div class="fb-comments" data-href="<?php echo $link?>" data-width="620" data-num-posts="10"></div>
+</div>
