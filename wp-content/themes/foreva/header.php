809dbd1f70fa499d68e08bc7c61849c8c3720b00
Hai Ly Thanh <nicka1711@gmail.com>
2013-08-26 15:16:34 +0700

    modify title homepage

--- wp-content/themes/foreva/header.php
+++ wp-content/themes/foreva/header.php
@@ -12,7 +12,7 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>" />
         <meta name="viewport" content="width=device-width" />
-        <title><?php wp_title('|', true, 'right'); ?></title>
+        <title><?php is_home() ? bloginfo('name') : wp_title('|', true, 'right'); ?></title>
         <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
         <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
         <!--[if lt IE 9]>
