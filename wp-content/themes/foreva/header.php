16edce3b723cd6b89305a3c1f33698b86fc3d525
Hai Ly Thanh <nicka1711@gmail.com>
2013-10-25 15:50:11 +0700

    add script chong copy

--- wp-content/themes/foreva/header.php
+++ wp-content/themes/foreva/header.php
@@ -58,6 +58,23 @@
             })();
 
         </script>
+        <!-- BEGIN Tynt Script -->
+        <script type="text/javascript">
+            if (document.location.protocol == 'http:') {
+                var Tynt = Tynt || [];
+                Tynt.push('dq2Zsspver4404acwqm_6r');
+                (function() {
+                    var s = document.createElement('script');
+                    s.async = "async";
+                    s.type = "text/javascript";
+                    s.src = 'http://tcr.tynt.com/ti.js';
+                    var h = document.getElementsByTagName('script')[0];
+                    h.parentNode.insertBefore(s, h);
+                })();
+            }
+        </script>
+        <!-- END Tynt Script -->
+
     </head>
 
     <body>
